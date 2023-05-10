7de76cfbe8f2f82c455c4df5853719a15e150eb7
YoussefAyachi95 <youssef.ayachi@web.de>
2023-05-10 15:41:45 +0200

    Add new category schema

--- api/controllers/menuControllers.js
+++ api/controllers/menuControllers.js
@@ -1,8 +1,14 @@
 require('../connections/connection')
-// const Category = require('../models/Category') -> Models
+const Category = require('../models/Category')
 
 //Get Homepage
 
-exports.homepage = async(req,res) => {
-    res.send('Hello')
-}
+exports.homepage = async (req, res) => {
+  try {
+    const categories = await Category.find({});
+    res.status(200).json(categories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Ein Fehler ist aufgetreten.');
+  }
+};
