e2a2b73f92aaa8dcbe7e4fa0b6131c49800a2a9f
Silte1 <etiennesilue3@gmail.com>
2023-05-12 09:48:22 +0200

    component, assets, und pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,45 @@
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+  Outlet
+} from "react-router-dom";
+import Home from "./pages/home/Home";
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App () {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
 
+
+      </Route>
+    )
+  )
   return (
-    <>
-      <h1>Tallo</h1>
-    </>
+    <main id="App" className="row m-0" >
+      <aside id="appNavigation" className="col-12 border bg-dark text-light text-center">
+        navigation
+      </aside>
+      <RouterProvider router={router} />
+
+      <footer id="appFooter" className="col-12 border bg-dark text-light text-center">
+        Footer
+      </footer>
+    </main>
+
+  )
+}
+
+const Root = () => {
+  return (
+    <section id="appBody" className="container col-12  bg-danger ">
+      <Outlet />
+    </section>
+
   )
 }
 
