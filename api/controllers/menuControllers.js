39627486d0beba962c36491eff0738795276b8ea
Michelle1404 <michelle.wollowski1404@gmail.com>
2023-05-15 10:02:30 +0200

    add new Category routes

--- api/controllers/menuControllers.js
+++ api/controllers/menuControllers.js
@@ -1,11 +1,11 @@
-require('../connections/connection')
+require("../connections/connection");
 
-const Category = require('../models/Category')
-const Menu = require('../models/Menu')
-const User = require('../models/Users')
+const Category = require("../models/Category");
+const Menu = require("../models/Menu");
+const User = require("../models/Users");
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //Admin created
 
@@ -30,7 +30,7 @@ exports.homepage = async (req, res) => {
     res.status(200).json(categories);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Ein Fehler ist aufgetreten.');
+    res.status(500).send("Ein Fehler ist aufgetreten.");
   }
 };
 
@@ -41,26 +41,78 @@ exports.homepageById = async (req, res) => {
     const categories = await Category.findOne({ _id: req.params.id });
 
     if (!categories) {
-      return res.status(404).send('Categories not found')
+      return res.status(404).send("Categories not found");
     }
 
     res.status(200).json(categories);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Ein Fehler ist aufgetreten.');
+    res.status(500).send("Ein Fehler ist aufgetreten.");
   }
 };
 
+//Post Category create
+
+exports.createCategory = async (req, res) => {
+  try {
+    const { name, image } = req.body;
+
+    const category = new Category({
+      name,
+      image,
+    });
+
+    await category.save();
+
+    res.status(201).json(category);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Ein Fehler ist aufgetreten.");
+  }
+};
+
+// Put Category update
+
+exports.updateCategory = async (req, res) => {
+  try {
+    const { name, image } = req.body;
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      req.params.id,
+      { name, image },
+      { new: true }
+    );
+
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Ein Fehler ist aufgetreten.");
+  }
+};
+
+//Delete a category
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await Category.findByIdAndDelete(id);
+
+    res.status(200).json(category);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Ein Fehler ist aufgetreten.");
+  }
+};
 
 //Get Menu
 
 exports.menu = async (req, res) => {
   try {
-    const menu = await Menu.find({}).populate('category');
+    const menu = await Menu.find({}).populate("category");
     res.status(200).json(menu);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Ein Fehler ist aufgetreten.');
+    res.status(500).send("Ein Fehler ist aufgetreten.");
   }
 };
 
@@ -68,16 +120,16 @@ exports.menu = async (req, res) => {
 
 exports.menuById = async (req, res) => {
   try {
-    const menu = await Menu.findById(req.params.id).populate('category');
+    const menu = await Menu.findById(req.params.id).populate("category");
 
     if (!menu) {
-      return res.status(404).send('Menu not found')
+      return res.status(404).send("Menu not found");
     }
 
     res.status(200).json(menu);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Ein Fehler ist aufgetreten.');
+    res.status(500).send("Ein Fehler ist aufgetreten.");
   }
 };
 
@@ -85,8 +137,7 @@ exports.menuById = async (req, res) => {
 
 exports.createMenu = async (req, res) => {
   try {
-
-    const { name, description, price, image, vegan, category } = req.body
+    const { name, description, price, image, vegan, category } = req.body;
 
     const menu = new Menu({
       name,
@@ -94,81 +145,74 @@ exports.createMenu = async (req, res) => {
       price,
       image,
       vegan,
-      category
-    })
+      category,
+    });
 
-    await menu.save()
+    await menu.save();
 
-    res.status(201).json(menu)
-
+    res.status(201).json(menu);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Ein Fehler ist aufgetreten.');
+    res.status(500).send("Ein Fehler ist aufgetreten.");
   }
-}
+};
 
 //Update Menu
 
 exports.updateMenu = async (req, res) => {
   try {
-
-    const { name, description, price, image, vegan, category } = req.body
+    const { name, description, price, image, vegan, category } = req.body;
 
     const updatedMenu = await Menu.findByIdAndUpdate(
       req.params.id,
       { name, description, price, image, vegan, category },
       { new: true }
-    ).populate('category');
+    ).populate("category");
 
-    res.status(200).json(updatedMenu)
-
+    res.status(200).json(updatedMenu);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Ein Fehler ist aufgetreten.');
+    res.status(500).send("Ein Fehler ist aufgetreten.");
   }
-}
+};
 
 //Delete Menu
 
 exports.deleteMenu = async (req, res) => {
   try {
+    const { id } = req.params;
+    const menu = await Menu.findByIdAndDelete(id);
 
-    const { id } = req.params
-    const menu = await Menu.findByIdAndDelete(id)
-
-    res.status(200).json(menu)
-
+    res.status(200).json(menu);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Ein Fehler ist aufgetreten.');
+    res.status(500).send("Ein Fehler ist aufgetreten.");
   }
-}
-
+};
 
 //Post login
 
 exports.login = async (req, res) => {
-
   const { email, password } = req.body;
 
   try {
-
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ message: 'Invalid user' });
+      return res.status(401).json({ message: "Invalid user" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid password' });
+      return res.status(401).json({ message: "Invalid password" });
     }
 
-    const token = jwt.sign({ userId: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { userId: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET
+    );
     return res.json({ token });
-        
   } catch (error) {
     console.error(error);
-    res.status(500).send('Ein Fehler ist aufgetreten.');
+    res.status(500).send("Ein Fehler ist aufgetreten.");
   }
 };
-
--- api/routes/menuRoutes.js
+++ api/routes/menuRoutes.js
@@ -1,21 +1,22 @@
-const express = require('express')
-const router = express.Router()
-const menuController = require('../controllers/menuControllers')
+const express = require("express");
+const router = express.Router();
+const menuController = require("../controllers/menuControllers");
 
-router.get('/menu', menuController.menu)
-router.get('/menu/:id', menuController.menuById)
-router.post('/menu', menuController.createMenu)
-router.put('/menu/:id', menuController.updateMenu)
-router.delete('/menu/:id', menuController.deleteMenu)
+router.get("/menu", menuController.menu);
+router.get("/menu/:id", menuController.menuById);
+router.post("/menu", menuController.createMenu);
+router.put("/menu/:id", menuController.updateMenu);
+router.delete("/menu/:id", menuController.deleteMenu);
 
-router.get('/', menuController.homepage)
-router.get('/:id', menuController.homepageById)
+router.get("/", menuController.homepage);
+router.get("/:id", menuController.homepageById);
+router.post("/", menuController.createCategory);
+router.put("/:id", menuController.updateCategory);
+router.delete("/:id", menuController.deleteCategory);
 
-router.post('/login', menuController.login)
+router.post("/login", menuController.login);
 
 // router.post('/search', recipeController.searchRecipe)
 // comment
 
-
-
-module.exports = router
+module.exports = router;
