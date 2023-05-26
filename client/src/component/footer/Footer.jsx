15a8bff9de5b3088a3cb57e54902b0734e430722
othman922 <aabo.bilal.955@gmial.com>
2023-05-26 18:01:46 +0200

    update h und f 70%

--- client/src/component/footer/Footer.jsx
+++ client/src/component/footer/Footer.jsx
@@ -1,72 +1,113 @@
 import { NavLink } from "react-router-dom";
 import "./Footer.css";
-import { FaFacebook, FaInstagram , FaTwitter,FaMapMarkerAlt} from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaMapMarkerAlt,
+  FaMobileAlt
+} from "react-icons/fa";
+import { HiOutlineMailOpen } from 'react-icons/hi';
 
 export const Footer = () => {
   const googleMapsLink = `https://goo.gl/maps/UUPQsbFnk8CBGATp7`;
 
   return (
-    <footer className="Footer">
-      <div className="myFooter">
-        <div className="flex ">
-          <div>
-            <ul>
-              <h3 className="location"> 
-              <p><FaMapMarkerAlt />
-              </p> <p> <span className="app-initial" >S</span>tandort</p> </h3>
-              <li>
-                <NavLink
-                  to={googleMapsLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Singerstr.77 <br />
-                  10243 berlin
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h3>kontakt</h3>
-            <p>gggg</p>
-            <p>ffffffff</p>
-          </div>
+    <footer>
+      <div className="f-item-con">
+        <div className="app-info">
+          <span className="app-name">
+            <span className="app-initial">W</span>eb-
+            <span className="app-initial">W</span>aiters
+          </span>
           
-          <div>
-            <ul className="opening-hours">
-              <h3>Öffnungszeiten</h3>
-              <p>
-                {" "}
-                Mo.−Di. geschlossen <br />
-                Mi.–So. 17:00–00:00 Uhr
-              </p>
-
-             
-            </ul>
-          </div>
+          <ul>
+            <li>Uber uns</li>
+            <li>Speisekarte</li>
+            <li>Events</li>
+          </ul>
+          {/* <p>
+              
+             Lorem <strong>consectetur</strong> and{" "}
+            <strong>adipisicing elit.</strong> Nisi, repudiandae..
+          </p> */}
         </div>
-        <div className="footer-end">
-      <ul className="flex1">
-          <div className="social-icons">
-            <NavLink  to="/">
-              <FaFacebook className="facebook"/>
-            </NavLink>
-            <NavLink to="https://www.instagram.com/aabo_bilal/" target="_blank">
-              <FaInstagram className="instgram" />
-            </NavLink>
-            <NavLink to="/">
-              <FaTwitter />
-            </NavLink>
-           
-          </div>
-        </ul> 
-      </div>
-      </div>
+       
 
-      {/* 
-        */}
-     
-     
+        <div className="g-i-t">
+          <div className="footer-title">Öffnungszeiten</div>
+          <ul>
+            <li> Mo.–So.</li>
+            <li>17:00–00:00 Uhr</li>
+            <li><h6>
+            letzter Einlass um 22:00 Uhr</h6></li>
+          </ul>
+        </div>
+        <div>
+        <div className="g-i-t">
+        <div className="footer-title">Address</div>
+          <ul>
+            <li> <NavLink  to={googleMapsLink}
+                  target="_blank"
+                  rel="noopener noreferrer" >
+              <span><FaMapMarkerAlt /></span> Straßer 77 Berlin 1044
+
+            </NavLink>
+            </li>
+            <li> <NavLink  to={googleMapsLink}
+                  target="_blank"
+                  rel="noopener noreferrer" >
+              <span><HiOutlineMailOpen /></span> mail@1234567.com
+
+            </NavLink>
+            </li>
+            <li> <NavLink  to={googleMapsLink}
+                  target="_blank"
+                  rel="noopener noreferrer" >
+              <span><FaMobileAlt /></span>  +91 90904500112
+
+            </NavLink>
+            </li>
+          </ul>
+        </div>
+        
+        </div>
+        <div className="useful-links">
+          <div className="footer-title">contact us</div>
+          <ul>
+          {/* <ul className="flex1">
+            <div className="social-icons">
+              <NavLink to="/">
+                <FaFacebook className="facebook" />
+              </NavLink>
+              <NavLink
+                to="https://www.instagram.com/aabo_bilal/"
+                target="_blank"
+              >
+                <FaInstagram className="instgram" />
+              </NavLink>
+              <NavLink to="/">
+                <FaTwitter />
+              </NavLink>
+            </div>
+          </ul> */}
+            <li> <NavLink to="/">
+                <FaFacebook className="facebook" />
+              </NavLink> Facebook</li>
+            <li><NavLink
+                to="https://www.instagram.com/aabo_bilal/"
+                target="_blank"
+              >
+                <FaInstagram className="instgram" />
+              </NavLink>{" "}Sign In</li>
+            <li>  <NavLink to="/">
+                <FaTwitter className="twitter"/>
+              </NavLink>About Us</li>
+          </ul>
+        </div>
+      </div> 
+      
+      <div className="cr-con">Copyright &copy; 2023 | Made by DCI</div>
     </footer>
   );
 };
